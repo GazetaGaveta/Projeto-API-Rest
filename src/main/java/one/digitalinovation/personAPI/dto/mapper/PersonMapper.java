35c0530b17cc952c1e83ca8913c0bc9d7beac6d4
GazetaGaveta <godoyjmb@gmail.com>
2021-06-18 17:08:42 -0300

    Aula 7 e Aula 8 parte 1
    Classes PersonDTO e PhoneDTO criadas. PersonDTO validando cpf. PersonControler valida dados recebidos e o método createPerson recebe uma PersonDTO ou invés de uma Person. Interface PersonMapper criada e usada para mapear uma Person em uma PersonDTO e vice-versa. Método de PersonService, createPerson modificado e recebendo uma PersonDTO. O pom.xml foi modificado para as validações funcionarem.

--- src/main/java/one/digitalinovation/personAPI/dto/mapper/PersonMapper.java
+++ src/main/java/one/digitalinovation/personAPI/dto/mapper/PersonMapper.java
@@ -1,4 +1,18 @@
 package one.digitalinovation.personAPI.dto.mapper;
 
+import one.digitalinovation.personAPI.dto.request.PersonDTO;
+import one.digitalinovation.personAPI.entities.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
 }
--- src/main/java/one/digitalinovation/personAPI/services/PersonService.java
+++ src/main/java/one/digitalinovation/personAPI/services/PersonService.java
@@ -1,5 +1,7 @@
 package one.digitalinovation.personAPI.services;
 
+import one.digitalinovation.personAPI.dto.mapper.PersonMapper;
+import one.digitalinovation.personAPI.dto.request.PersonDTO;
 import one.digitalinovation.personAPI.dto.response.MessageResponseDTO;
 import one.digitalinovation.personAPI.entities.Person;
 import one.digitalinovation.personAPI.repositories.PersonRepository;
@@ -11,13 +13,17 @@ public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
